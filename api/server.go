36e6fb8cbbaa4a0033ed4cd98bf902fd5daf8e74
thinh bui <thinhbuihong.ma@gmail.com>
2022-05-18 00:13:27 +0700

    feat: transfer api + register custom validator

--- api/server.go
+++ api/server.go
@@ -2,6 +2,8 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 	db "github.com/thinhbuihong/simplebank/db/sqlc"
 )
 
@@ -14,6 +16,10 @@ func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
+
 	//add routes to router
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
@@ -21,6 +27,8 @@ func NewServer(store db.Store) *Server {
 	router.PATCH("/accounts/:id/add", server.addBalance)
 	router.PATCH("/accounts/:id", server.updateAccount)
 	router.DELETE("/accounts/:id", server.deleteAccount)
+
+	router.POST("/transfers", server.createTransfer)
 
 	server.router = router
 	return server
