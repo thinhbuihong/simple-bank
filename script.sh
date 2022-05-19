65b8151ab2d0861111f1182fbbe8bdb7c31242cf
thinh bui <thinhbuihong.ma@gmail.com>
2022-05-19 18:34:33 +0700

    feat: add migration file to create user table

--- script.sh
+++ script.sh
@@ -2,13 +2,13 @@
 
 case $1 in 
 	"migrate:up")
-		migrate -path db/migrations -database "postgres://postgres:buihongthinh@localhost:5432/simplebank?sslmode=disable" -verbose up
+		migrate -path db/migrations -database "postgres://postgres:buihongthinh@localhost:5432/simplebank?sslmode=disable" -verbose up ${@:2}
 		;;
 	"migrate:down")
-		migrate -path db/migrations -database "postgres://postgres:buihongthinh@localhost:5432/simplebank?sslmode=disable" -verbose down
+		migrate -path db/migrations -database "postgres://postgres:buihongthinh@localhost:5432/simplebank?sslmode=disable" -verbose down ${@:2}
 		;;
 	"migrate:create")
-		migrate create -ext sql -dir db/migrations -seq $2
+		migrate create -ext sql -dir db/migrations -seq ${@:2}
 		;;
 	"mockdb")
 		mockgen -package mockdb -destination db/mockdb/store.go --build_flags=--mod=mod github.com/thinhbuihong/simplebank/db/sqlc Store
