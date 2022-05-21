d9fba40493d1a0b1dc0af25bcaab590eb0214e10
thinh bui <thinhbuihong.ma@gmail.com>
2022-05-21 19:28:38 +0700

    test: test for user api

--- api/main_test.go
+++ api/main_test.go
@@ -7,6 +7,18 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// func newTestServer(t *testing.T, store db.Store) *Server {
+// 	config := util.Config{
+// 		TokenSymmetricKey:   util.RandomString(32),
+// 		AccessTokenDuration: time.Minute,
+// 	}
+
+// 	server, err := NewServer(config, store)
+// 	require.NoError(t, err)
+
+// 	return server
+// }
+
 func TestMain(m *testing.M) {
 	gin.SetMode(gin.TestMode)
 	os.Exit(m.Run())
